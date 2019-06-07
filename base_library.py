2859433c0149d8027cf6de89a852b944899bc7d1
apple <apple@114-42-11-209.dynamic-ip.hinet.net>
2019-06-07 21:29:39 +0800

    show the version of basic libries

--- base_library.py
+++ base_library.py
@@ -8,3 +8,19 @@
     . pandas: It is a Python library for data wrangling and analysis.
 '''
 import os
+import sys
+import numpy as np
+import scipy as sp
+import pandas as pd
+import matplotlib
+import IPython
+import sklearn
+
+# show the version of each library
+print('Python version: {}'.format(sys.version))
+print('Numpy version: {}'.format(np.__version__))
+print('Scipy version: {}'.format(sp.__version__))
+print('Pandas version: {}'.format(pd.__version__))
+print('Matplotlib version: {}'.format(matplotlib.__version__))
+print('IPython version: {}'.format(IPython.__version__))
+print('scikit-learn version: {}'.format(sklearn.__version__))
