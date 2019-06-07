eeeb87eace41f5e202fb0b48766bb254889ab7d0
apple <apple@114-42-11-209.dynamic-ip.hinet.net>
2019-06-07 21:03:03 +0800

    Try the 2nd to push code into github

--- base_library.py
+++ base_library.py
@@ -1,4 +1,9 @@
 ''' Basic libraries
     . scikit-learn: It contains a lot of state-of-the-art machine learning
       algorithms.
+
+    . Numpy: It is the fundamental packages for scientific computing in Python.
+    . Scipy: It is a collection of functions for scientific computing in Python.
+    . marplotlib: It is the primary scientific plotting library in Python.
+    . pandas: It is a Python library for data wrangling and analysis.
 '''
